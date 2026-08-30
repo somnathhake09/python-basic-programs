b5b8e81755da90febd4d1b10c39fb80c1c6e43fb
somnathhake09 <somnathhake09@gmail.com>
2026-08-30 16:13:31 +0530

    add a python Opps concept Instance methodsClass Attributes vs Instance Attribute

--- Oopps_Concept.py
+++ Oopps_Concept.py
@@ -10,3 +10,19 @@ class Transaction:
 
 t1 = Transaction("Groceries", 1200, "Food")
 t1.display()   # Groceries: 1200 (Food)
+
+
+# Class Attributes vs Instance Attributes
+class Transaction:
+    essential_categories = ("Food", "Rent")   # ← Class attribute —  objects SHARED
+
+    def __init__(self, description, amount, category):
+        self.description = description   # ← Instance attribute —  object 
+        self.amount = amount
+        self.category = category
+
+t1 = Transaction("Groceries", 1200, "Food")
+t2 = Transaction("Movie", 350, "Entertainment")
+
+print(Transaction.essential_categories)   # ("Food", "Rent") —  access 
+print(t1.essential_categories)            
